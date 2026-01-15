be33c51844ddaba8c20a57b4f8c1ab9482634eb9
Maciej <maciek.widlak@gmail.com>
2026-01-15 12:18:54 +0100

    v2 - extracting audio from video

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# subtitle_generator
+# Video Subtitle Generator
+
+### Aplikacja webowa do automatycznego generowania napisów z plików wideo z wykorzystaniem modeli speech-to-text.
+
+#### Projekt umożliwia użytkownikowi wgranie pliku wideo, automatyczne wyodrębnienie ścieżki audio, transkrypcję mowy na tekst oraz wyświetlenie wideo zsynchronizowanego z wygenerowanymi napisami. Celem aplikacji jest szybkie i dostępne tworzenie napisów bez potrzeby ręcznej edycji.
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-import pandas as pd
 from dotenv import dotenv_values
 from openai import OpenAI
 from hashlib import md5
-from io import BytesIO
+from pydub import AudioSegment
+import tempfile
+
 
 AUDIO_TRANSCRIBE_MODEL = "whisper-1"
 env = dotenv_values(".env")
@@ -49,7 +50,22 @@ if "video_bytes" not in st.session_state:
 
 st.title("SUBTITLE GENERATOR")
 
-uploaded_file = st.file_uploader("Prześlij plik do transkrypcji", type=["mp3", "mp4", "m4a", "wav"], key="video_file")
+uploaded_file = st.file_uploader("Send a file for transcription", type=["mp3", "mp4", "m4a", "wav"], key="video_file")
 if uploaded_file is not None:
     video_bytes = uploaded_file.read()
     st.video(video_bytes, format="video/mp4", width="stretch")
+
+    # Save uploaded video to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video_file:
+        temp_video_file.write(video_bytes)
+        temp_video_path = temp_video_file.name
+
+    # Convert video to audio
+    audio = AudioSegment.from_file(temp_video_path, format="mp4")
+
+      # Save audio to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio_file:
+        audio.export(temp_audio_file.name, format="mp3")
+        st.success("Audio was extracted.")
+        st.audio(temp_audio_file.name, format="audio/mp3")
+    
